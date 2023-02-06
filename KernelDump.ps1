dc983d4ed79ee6c8f008e12101510a77bb70d5a4
conexioninversa - Pedro Sánchez Cordero <pedro.sanchez@conexioninversa.com>
2023-02-06 18:02:56 +0100

    Update KernelDump.ps1

--- KernelDump.ps1
+++ KernelDump.ps1
@@ -13,8 +13,24 @@
 param (
     [string]$outputPath = "$env:temp\"
 )
+# returning class instances:
+Get-CimInstance -ClassName CIM_DataFile -Filter "Drive='c:' and Path='\\windows\\system32\\'" | Select-Object -Property Name, LastAccessed
+
+# reading property value:
+Get-CimInstance -ClassName CIM_DataFile -Filter "Drive='c:' and Path='\\windows\\system32\\'" | Select-Object -Property Name, LastAccessed | Foreach-Object {
+
+  $Name = $_.Name
+  $value = $_.LastAccessed
+  "${Name}: LastAccessed = $value"
+}
+
+(Get-CimInstance -Class Win32_OperatingSystem).InstallDate
+Get-CimInstance -ClassName Win32_OperatingSystem | select LastBootUpTime
+gcim win32_process -filter "name='svchost.exe'" | select commandline, name
+
 $ss = Get-CimInstance -ClassName MSFT_StorageSubSystem -Namespace Root\Microsoft\Windows\Storage;
 Invoke-CimMethod -InputObject $ss -MethodName "GetDiagnosticInfo" -Arguments @{DestinationPath=$outputPath; IncludeLiveDump=$true};
+
 
 # Replace "$env:temp\" with a route from your computer disk or add a route as a parameter to the script
 # Special thanks to Grzegorz Tworek - 0gtweet.
