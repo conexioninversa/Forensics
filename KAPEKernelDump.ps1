d97cbfae0781ae4806d4effa41319720a871a0f8
conexioninversa - Pedro Sánchez Cordero <pedro.sanchez@conexioninversa.com>
2023-02-07 15:41:27 +0100

    Create KAPEKernelDump.ps1

---
+++ KAPEKernelDump.ps1
@@ -0,0 +1,22 @@
+#██╗  ██╗███████╗██████╗ ███╗   ██╗███████╗██╗      ██████╗ ██╗   ██╗███╗   ███╗██████╗ 
+#██║ ██╔╝██╔════╝██╔══██╗████╗  ██║██╔════╝██║      ██╔══██╗██║   ██║████╗ ████║██╔══██╗
+#█████╔╝ █████╗  ██████╔╝██╔██╗ ██║█████╗  ██║█████╗██║  ██║██║   ██║██╔████╔██║██████╔╝
+#██╔═██╗ ██╔══╝  ██╔══██╗██║╚██╗██║██╔══╝  ██║╚════╝██║  ██║██║   ██║██║╚██╔╝██║██╔═══╝  
+#██║  ██╗███████╗██║  ██║██║ ╚████║███████╗███████╗ ██████╔╝╚██████╔╝██║ ╚═╝ ██║██║      
+#╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝╚══════╝ ╚═════╝  ╚═════╝ ╚═╝     ╚═╝╚═╝     
+                                                                                       
+
+# This script allows you to dump the Kernel part of the ram without using any external tool
+# Where the Microsoft Storage namespace is available (known not to be available in Win7), PowerShell can be used to invoke a native live memory dump
+# This example has been modified to work with KAPE by Eric Zimmmerman
+
+param (
+    [string]$outputPath = "$env:temp\"
+)
+
+$me = Get-CimInstance -ClassName MSFT_StorageSubSystem -Namespace Root\Microsoft\Windows\Storage;
+Invoke-CimMethod -InputObject $me -MethodName "GetDiagnosticInfo" -Arguments @{DestinationPath=$outputPath; IncludeLiveDump=$true};
+
+
+# Replace "$env:temp\" with a route from your computer disk or add a route as a parameter to the script
+# Special thanks to Grzegorz Tworek - 0gtweet.
