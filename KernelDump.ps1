9edb299204a07cf6f82b18162c3a3e9773f094bc
conexioninversa - Pedro Sánchez Cordero <pedro.sanchez@conexioninversa.com>
2023-02-07 15:43:08 +0100

    Update KernelDump.ps1

--- KernelDump.ps1
+++ KernelDump.ps1
@@ -7,8 +7,6 @@
                                                                                        
 
 # This script allows you to dump the Kernel part of the ram without using any external tool
-# Where the Microsoft Storage namespace is available (known not to be available in Win7), PowerShell can be used to invoke a native live memory dump
-# This example has been modified to work with KAPE by Eric Zimmmerman
 
 param (
     [string]$outputPath = "$env:temp\"
